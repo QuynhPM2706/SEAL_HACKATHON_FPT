9ea91c760dd6e3625f0b81c86868d7bbbc637e02
Tran Truc Vy <trantrucvy3@gmail.com>
2026-07-18 03:16:57 +0700

    feat(team): team enums

---
+++ backend/src/main/java/com/sealhackathon/team/domain/enums/InvitationStatus.java
@@ -0,0 +1,10 @@
+package com.sealhackathon.team.domain.enums;
+
+public enum InvitationStatus {
+
+    PENDING,
+    ACCEPTED,
+    REJECTED,
+    EXPIRED,
+    CANCELLED
+}
---
+++ backend/src/main/java/com/sealhackathon/team/domain/enums/TrackAssignmentMethod.java
@@ -0,0 +1,7 @@
+package com.sealhackathon.team.domain.enums;
+
+public enum TrackAssignmentMethod {
+    RANDOM,
+    MANUAL,
+    SELF_DRAW
+}
