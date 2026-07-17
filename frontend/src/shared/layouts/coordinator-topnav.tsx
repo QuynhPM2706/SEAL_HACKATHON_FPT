b529002fb10725b31f8d2aafb4efa515000bd5ef
Ving <nguyentruongvinh05@gmail.com>
2026-07-18 02:32:08 +0700

    feat(fe): shared layouts

---
+++ frontend/src/shared/layouts/coordinator-topnav.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import { PortalTopNav } from "@/shared/layouts/portal-topnav";
+
+export function CoordinatorTopNav() {
+  return <PortalTopNav searchPlaceholder="Search users, teams..." />;
+}
