332e4e735d3f33e41ddd0b7592e23759512acb8c
Phan Lại Phong <phanphong270105@gmail.com>
2026-07-18 02:47:16 +0700

    feat(event): event enums

---
+++ backend/src/main/java/com/sealhackathon/event/domain/enums/EventStatus.java
@@ -0,0 +1,12 @@
+package com.sealhackathon.event.domain.enums;
+
+public enum EventStatus {
+
+    UPCOMING,
+    OPEN,
+    CLOSED_REGISTRATION,
+    ACTIVE,
+    SCORING,
+    COMPLETED,
+    CANCELLED
+}
